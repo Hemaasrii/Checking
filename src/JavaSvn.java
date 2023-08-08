bedb6527b4cb3fe32e102482f3f8c1dcd8baedb2
Hemaasrii <hemasri1697@gmail.com>
2023-08-08 13:50:49 +0530

    Nan dan

---
+++ src/JavaSvn.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.Random;
+import java.util.IntSummaryStatistics;
+
+public class JavaSvn {
+	public static void main(String[] args) {
+		List<String> ref=Arrays.asList("Hema","Latha","SriMathi"," ");
+		System.out.println("List of Elements : " + ref);
+		
+	List<String> filter=deleteEmptyStringUsingJava7(ref)
+			System.out.println("Delete Empty String : " + filter);
+	
+	
+	List<Integer> numbers=Arrays.asList(10,12,20,40);
+	List<Integer> sqare=getSquare
+
+		
+	}
+
+}
